e48854e1da7447f437ce161ec75bd9042c4e6140
StrawberSam <s.roche53@hotmail.com>
2025-04-22 10:58:07 +0200

    ✨(main.h) content : all prototypes of functions and add get_line() function

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int get_line(void);
+
+
+#endif /* MAIN_H */
