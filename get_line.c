8721814b75e9bafa0816ac2a67ff32639ab0d9af
StrawberSam <s.roche53@hotmail.com>
2025-04-22 15:18:40 +0200

    :hammer: (get_line.c) : Fix feod with errno and fix the name of the prototype adding * and changing the int with the char

--- get_line.c
+++ get_line.c
@@ -32,7 +32,7 @@ void cleaner(char *line)
  *
  * Return: Always 0 (Success)
  */
-int get_line(void)
+char *get_line(void)
 {
 	/*pointeur vers chaine contenant la ligne de l'utilisateur*/
 	char *line = NULL;
@@ -40,6 +40,7 @@ int get_line(void)
 	size_t len = 0;
 	/*stock le nombre de caractère lu par getline*/
 	ssize_t read;
+	errno = 0;/*initialise errno à zéro pour éviter d'anciennes valeurs*/
 
 	/*permet une boucle infinie tant que l'utilisateur ne tape pas EOF*/
 	printf("$ ");/*affiche prompt et attend la ligne de l'utilisateur*/
@@ -48,18 +49,17 @@ int get_line(void)
 	if (read == -1)
 	{
 /*L'utilisateur tape EOF. Fin de fichier détectée. Le programme se termine*/
-		if (feof(stdin))
-		{
+		if (errno == 0)/*variable globale qui contient code erreur d'une fonc*/
+		{/*0 = pas d'erreur*/
 			exit (0);
 		}
 		else/*Erreur de lecture*/
 		{
-			printf(": command not found.");
-			return (-1);
+			perror(": command not found.");
+			exit (1);
 		}
 	}
 	cleaner(line);/*appel fonction: nettoyage du \n et si line non valide*/
 
-	free(line);/*free car getline*/
-	return (0);
+	return (line);
 }
