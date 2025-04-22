c902949c64df7e6d69ed02d916cc199e88925307
StrawberSam <s.roche53@hotmail.com>
2025-04-22 15:21:41 +0200

    :hammer: (main.h) : Fix the prototype of get_line and add new library for errno

--- main.h
+++ main.h
@@ -7,8 +7,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
-int get_line(void);
+char *get_line(void);
 int main(void);
 char **split_line(char *line);
 
