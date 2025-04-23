2bce7065d0a29c6b013c855071203ef58e7b96cf
Helvlaska <ccastan@live.fr>
2025-04-23 10:39:22 +0200

    :hammer: (main.h) : Add prototype of _which() function

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -12,5 +13,7 @@
 char *get_line(void);
 int main(void);
 char **split_line(char *line);
+char *find_command_in_path(const char *cmd);
+
 
 #endif /* MAIN_H */
