7cc7aecd90a6daabfa37ce772a35b24ba9aafa0d
StrawberSam <s.roche53@hotmail.com>
2025-04-23 16:32:45 +0200

    :adhesive_bandage: (all files) : repare branch main

---
+++ get_line.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * cleaner - nettoyage du \n
+ *
+ * @line: string
+ */
+
+void cleaner(char *line)
+{
+	int i;
+
+	if (line == NULL)/*Vérifie que line n'est pas NULL*/
+	{
+		return;/*Si line est NULL, on ne fait rien*/
+	}
+
+	for (i = 0; line[i] != '\0'; i++)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = '\0';
+			return;
+		}
+	}
+}
+
+/**
+ * main - affiche un prompt "$ ", attend une commande
+ * de l'utilisateur, puis l'affiche.
+ * Il s'arrête proprement à la fin de fichier (Ctrl+D).
+ *
+ * Return: Always 0 (Success)
+ */
+char *get_line(void)
+{
+	/*pointeur vers chaine contenant la ligne de l'utilisateur*/
+	char *line = NULL;
+	/*taille initiale du buffer, getline ajuste la taille automatiquement*/
+	size_t len = 0;
+	/*stock le nombre de caractère lu par getline*/
+	ssize_t read;
+	errno = 0;/*initialise errno à zéro pour éviter d'anciennes valeurs*/
+
+	/*permet une boucle infinie tant que l'utilisateur ne tape pas EOF*/
+	if (isatty(STDIN_FILENO))/*interactive mode*/
+	{
+		printf("$ ");/*affiche prompt et attend la ligne de l'utilisateur*/
+	}
+	read = getline(&line, &len, stdin);
+
+	if (read == -1)
+	{
+		free(line);
+/*L'utilisateur tape EOF. Fin de fichier détectée. Le programme se termine*/
+		if (errno == 0)/*variable globale qui contient code erreur d'une fonc*/
+		{/*0 = pas d'erreur*/
+			exit (0);
+		}
+		else/*Erreur de lecture*/
+		{
+			perror(": command not found.");
+			exit (1);
+		}
+	}
+	cleaner(line);/*appel fonction: nettoyage du \n et si line non valide*/
+
+	return (line);
+}
---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+void cleaner(char *line);
+char *get_line(void);
+char **split_line(char *line);
+char *find_command_in_path(const char *cmd);
+int main(void);
+
+#endif /* MAIN_H */
