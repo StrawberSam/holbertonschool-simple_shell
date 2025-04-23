4e4637cad0898da0326e91df6a57810de2983f6b
StrawberSam <s.roche53@hotmail.com>
2025-04-23 21:37:51 +0200

    :hammer: (main.h) : Add new prototype for the new function "handle_no_args_command.c"

--- main.h
+++ main.h
@@ -15,5 +15,6 @@ char *get_line(void);
 char **split_line(char *line);
 char *find_command_in_path(const char *cmd);
 int main(int ac, char **av);
+void handle_no_args_command(char *line);
 
 #endif /* MAIN_H */
