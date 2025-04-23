44ceb03784007c9496fee0b47681df124083e912
StrawberSam <s.roche53@hotmail.com>
2025-04-24 01:33:55 +0200

    :hammer: :sparkles: (main.h) : Add new prototype for new functions in main.c (handle_exit, handle_env, execute_command, handle_simple_command)

--- main.h
+++ main.h
@@ -10,11 +10,16 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+extern char **environ;
 void cleaner(char *line);
 char *get_line(void);
 char **split_line(char *line);
 char *find_command_in_path(const char *cmd);
 int main(int ac, char **av);
 void handle_no_args_command(char *line);
+int handle_exit(char **args);
+void handle_env();
+void execute_command(char **args, pid_t pid, char **environ, char *line);
+void handle_simple_command(char *line);
 
 #endif /* MAIN_H */
