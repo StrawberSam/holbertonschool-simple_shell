0d4baccc3f3aeecfee93ea3cf97ac78b8a7c34b0
StrawberSam <s.roche53@hotmail.com>
2025-04-24 14:06:04 +0200

    :hammer: Add new functions in main.c (handle_read_error for EOF : ctrl+d)

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 /**
  * handle_exit - Vérifie si la commande est "exit"
  * @args: Tableau d'arguments
- *
  * Return: 1 si c'est "exit", 0 sinon
  */
 int handle_exit(char **args)
@@ -15,13 +14,9 @@ int handle_exit(char **args)
 
 /**
  * handle_env - Affiche les variables d'environnement
- *
- * @args: Un tableau d'arguments passés à la commande
- * @line: La ligne d'entrée à libérer
- *
  * Return: Aucun, car la fonction retourne void.
  */
-void handle_env()
+void handle_env(void)
 {
 	int i = 0;
 
@@ -33,33 +28,57 @@ void handle_env()
 }
 
 /**
+ * handle_read_error - Gère les erreurs de la fonction read, dont EOF (Ctrl+D)
+ * @line: ligne à libérer en cas d'erreur
+ * @read: valeur de retour de la fonction getline
+ * Libère la mémoire, détecte si EOF est atteint (errno == 0) ou erreur
+ */
+void handle_read_error(char *line, ssize_t read)
+{
+	if (read == -1)
+	{
+		free(line);
+
+		if (errno == 0)
+		{
+			/* EOF (Ctrl+D) sans erreur → on quitte normalement */
+			exit(0);
+		}
+		else
+		{
+			/* Autre erreur → message d'erreur + sortie */
+			perror(": command not found.");
+			exit(1);
+		}
+	}
+}
+
+/**
  * main - Point d'entrée du programme shell.
  * @ac: Nombre d'arguments.
  * @av: Tableau des arguments passés au programme.
- *
  * Return: Toujours 0 (Succès).
  */
 int main(int ac, char **av)
 {
 	char *line = NULL;  /* Stocke la ligne d'entrée de l'utilisateur */
 	size_t len = 0;
+	ssize_t read;
 	char **args;        /* Stocke les tokens extraits de la ligne d'entrée */
-	(void)ac; /* Paramètre inutilisé */
-	(void)av;
+	(void)ac, (void)av; /* Paramètre inutilisé */
+	errno = 0;
 
 	while (1)
 	{
-		/* Si le programme est en mode interactif, afficher l'invite */
-		if (isatty(STDIN_FILENO))
-		{
+		if (isatty(STDIN_FILENO))/*si prog interactif, affiche l'invite*/
 			printf("$ ");
-			fflush(stdout);
-		}
 		else
 			handle_non_interactive(av);
 
-		getline(&line, &len, stdin);  /* Récupérer la ligne d'entrée */
+		read = getline(&line, &len, stdin);  /* Récupérer la ligne d'entrée */
+		handle_read_error(line, read);
 		cleaner(line);
+
 		if (line == NULL)
 			continue;
 
@@ -70,23 +89,16 @@ int main(int ac, char **av)
 			continue;
 		}
 
-		/* Gérer les commandes comme exit et env */
-		if (handle_exit(args))
+		if (handle_exit(args))/* Gérer les commandes comme exit et env */
 		{
 			free(line);
 			free(args);
 			exit(0);
 		}
 		else if (strcmp(args[0], "env") == 0)
-		{
-			handle_env(args, line);
-		}
+			handle_env();
 		else
-		{
 			execute_command(args, av); /* Exécuter les autres cmd */
-		}
-
 	}
-
 	return (0);
 }
