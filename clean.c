5ad99dc2c202530df9fd2f18a84a8e4fa6a4a938
Helvlaska <ccastan@live.fr>
2025-04-24 21:09:44 +0200

    :hammer: (clean.c) : Add command and clean syntax of file

--- clean.c
+++ clean.c
@@ -1,32 +1,36 @@
 #include "main.h"
+
 /**
- * cleanup - Libère la mémoire allouée pour la ligne et les arguments
- * @line: Ligne lue
- * @args: Arguments de la commande
+ * cleanup - Libère la mémoire allouée pour la cmd et l'array de mots
+ * @line: commande lue
+ * @args: tableau de mots
  */
 void cleanup(char *line, char **args)
 {
-    free(line); /*libération de la mémoire de line*/
-    free(args); /*libération de la mémoire de l'array de mots*/
+	free(line); /*libération de la mémoire de la cmd*/
+	free(args); /*libération de la mémoire de l'array de mots*/
 }
+
 /**
  * cleaner - nettoyage du \n
  *
- * @line: string
+ * @line: string (la commande)
  */
 void cleaner(char *line)
 {
-    int i;
-    if (line == NULL)/*Vérifie que line n'est pas NULL*/
-    {
-        return;/*Si line est NULL, on ne fait rien*/
-    }
-    for (i = 0; line[i] != '\0'; i++)
-    {
-        if (line[i] == '\n')
-        {
-            line[i] = '\0';
-            return;
-        }
-    }
+	int i; /*variable de boucle par l'index*/
+
+	if (line == NULL) /*Vérifie que cmd n'est pas NULL*/
+	{
+		return; /*Si cmd est NULL, on ne fait rien*/
+	}
+	/*on avance sur les char de la cmd jusqu'à la fin*/
+	for (i = 0; line[i] != '\0'; i++)
+	{
+		if (line[i] == '\n') /*quand on arrive au charriot...*/
+		{
+			line[i] = '\0'; /*...on le remplace par le char de fin de string*/
+			return; /*on renvoie la modification*/
+		}
+	}
 }
