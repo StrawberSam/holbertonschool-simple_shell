6832184a5b63e0e1711b067eb235daeb92015672
Helvlaska <ccastan@live.fr>
2025-04-24 02:20:13 +0200

    :hammer: (main.h) : Add prototypes of all functions to non-interactive mode

--- main.h
+++ main.h
@@ -12,14 +12,19 @@
 
 extern char **environ;
 void cleaner(char *line);
+void cleanup(char *line, char **args);
+void handle_no_args_command(char *line);
+void handle_env();
+void execute_command(char **args, pid_t pid, char **environ, char *line);
+void handle_simple_command(char *line);
+void simple_command(char *line);
+void execute_command_interactive(char **args, char **av);
+void handle_non_interactive(char **av);
 char *get_line(void);
 char **split_line(char *line);
 char *find_command_in_path(const char *cmd);
 int main(int ac, char **av);
-void handle_no_args_command(char *line);
 int handle_exit(char **args);
-void handle_env();
-void execute_command(char **args, pid_t pid, char **environ, char *line);
-void handle_simple_command(char *line);
+ssize_t read_line(char **line, size_t *len);
 
 #endif /* MAIN_H */
